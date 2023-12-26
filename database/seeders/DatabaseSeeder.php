1d3138924c14d1a211ac9bd5cceb75a00559b08e
Eugene Zarianski <eugene.somemail.com>
2023-12-26 12:23:46 +0300

    add admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RateSeeder::class,
-            UserTypeSeeder::class
+            UserTypeSeeder::class,
+            AdminSeeder::class
         ]);
 
     }
