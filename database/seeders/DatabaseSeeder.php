ad3dafd8ef2c3c64f66a34279f59f7d2aab682d3
Eugene Zarianski <eugene.somemail.com>
2023-12-26 12:08:28 +0300

    add db init seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,56 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-
-        $user_types = ['admin', 'employee'];
-        $rates = 
-        [
-            ['rate_slug' => 'per_hour', 'rate_name' => 'Почасовая ставка'],
-            ['rate_slug' => 'per_week', 'rate_name' => 'Недельная ставка'],
-        ];
-
-        $user_params = [
-            'name' => 'administrator',
-            'email' => 'administrator@employes.com',
-            'password' => '12345qqq',
-            'type' => 'admin',
-        ];
-
-
-
-        foreach ($user_types as $user_type) {
-           UserType::create([
-                'user_type_name' => $user_type,
-           ]);
-        }
-
-        foreach ($rates as $rate) {
-            Rate::create([
-                'rate_slug' => $rate['rate_slug'],
-                'rate_name' => $rate['rate_name'],
-            ]);
-        }
-
-        $user = User::factory()->create([
-            'name' => $user_params['name'],
-            'email' => $user_params['email'],
-            'password' => $user_params['password'],
+        $this->call([
+            RateSeeder::class,
+            UserTypeSeeder::class
         ]);
-
-        $admin_type =  UserType::where('user_type_name', 'admin')->first();
-
-        UsersTypes::create([
-            'user_id' => $user->id,
-            'user_type_id' => $admin_type->id,
-        ]);
-
 
     }
 }
